6c4b418aefe11a89fa81bea8d0e1742e6b639f19
tanya selvog <tanyaSelvog@gmail.com>
2019-06-10 11:52:00 -0700

    updated deleting comments html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+   http_basic_authenticate_with name: "trs", password: "taco", except: [:index, :show]
+   
+   
     def index
         @articles = Article.all
     end
